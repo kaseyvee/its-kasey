43bac9dc66dba56c4f0125b814097d6498babe07
kethnal <kaseyvaldez@hotmail.com>
2023-04-28 03:22:56 -0700

    Build carousel

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,33 +1,103 @@
+import { useState } from "react";
+
+import { ReactComponent as ArrowLeft } from "../assets/icon-arrow-left.svg";
+import { ReactComponent as ArrowRight } from "../assets/icon-arrow-right.svg";
 import Divider from "../components/Divider";
 
+import { data } from "../data";
+
 function Projects() {
+  const projects = data.projects;
+
+  const [current, setCurrent] = useState(0)
+
+  const carouselButtons = projects.map(project => {
+
+    function handleGetStyle() {
+      if (current === projects.indexOf(project)) {
+        return "#121311";
+      }
+    }
+
+    const selected = handleGetStyle();
+
+    return (
+      <button
+        key={project.id + `123`}
+        className="projects_carousel-nav_button-group_item"
+        onClick={() => setCurrent(projects.indexOf(project))}
+        style={selected ? {backgroundColor: `${selected}`} : {}}
+      ></button>
+    )
+  })
+
+  function handleRotateProject(direction) {
+    if (direction === "next") {
+      if (current === projects.length - 1) return setCurrent(0);
+      return setCurrent(current + 1);
+    }
+
+    if (direction === "prev") {
+      if (current === 0) return setCurrent(projects.length - 1);
+      return setCurrent(current - 1);
+    }
+  }
+
   return (
     <section aria-labelledby="projects-title" id="projects" className="projects | container">
       <Divider position="top" colour="black" />
       <div className="wrapper">
         <h2 id="projects-title">Some things I&apos;ve made</h2>
 
-        <div className="projects_card">
+        <div className="projects_card" aria-label={`project ${current + 1} out of ${projects.length}`}>
           <div className="projects_card_images">
-            <img src="src/assets/projects/manage-1.png" alt="" className="projects_card_images_main" />
-            <img src="src/assets/projects/manage-2.png" alt="" className="projects_card_images_alt" />
+            <img
+              src={projects[current].img1}
+              alt=""
+              className="projects_card_images_main"
+            />
+
+            {projects[current].img2 &&
+            <img
+              src={projects[current].img2}
+              alt=""
+              className="projects_card_images_alt"
+            />}
           </div>
 
           <div className="projects_card_info">
             <header>
-              <h3>Manage Landing Page</h3>
+              <h3>{projects[current].title}</h3>
 
               <div className="project-links">
-                <span><a href="">Demo</a> | <a href="">Repo</a></span>
+                <span><a href={projects[current].demo}>Demo</a> | </span><a href={projects[current].repo}>Repo</a>
               </div>
             </header>
 
-            <p>This landing page was built using React, Sass, and Framer Motion, with a mobile-first workflow and web accessibility in mind. Web accessibility was a key consideration in the development process, making sure the landing page is inclusive and can be accessed by everyone.</p>
+            <p>{projects[current].description}</p>
           </div>
         </div>
 
         <div className="projects_carousel-nav">
+          <button
+            className="projects_carousel-nav_left"
+            aria-label="See previous project"
+            onClick={() => handleRotateProject("prev")}
+          >
+            <ArrowLeft />
+          </button>
 
+          <div className="projects_carousel-nav_button-group">
+            {carouselButtons}
+          </div>
+
+          <button
+            className="projects_carousel-nav_right"
+            aria-label="See next project"
+            onClick={() => handleRotateProject("next")}
+          >
+            <ArrowRight />
+          </button>
         </div>
       </div>
     </section>
