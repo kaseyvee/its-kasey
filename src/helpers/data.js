60ca4eb6d6717406619b87f530257ee18bbfaf9d
kethnal <kaseyvaldez@hotmail.com>
2023-05-02 06:38:14 -0700

    Change title

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -6,7 +6,7 @@ export const data = {
       id: 1,
       number: 1,
       title: "Collaboration",
-      description: "There’s no greater sense of satisfaction than witnessing ideas come to life through collaborative endeavours. Engaging in frequent discussions and exchanging ideas is a source of inspiration for me."
+      description: "There’s no greater sense of satisfaction than witnessing ideas come to life through collaborative endeavours. I'd always be happy to hop on calls hit the DMs to exchange ideas and thoughts!"
     },
     {
       id: 2,
@@ -18,7 +18,7 @@ export const data = {
       id: 3,
       number: 3,
       title: "Skill Upkeep",
-      description: "An organization that recognizes and prioritizes the personal and professional development of its employees is indicative of its commitment to serving its clients and promoting the well-being of its staff."
+      description: "An organization that recognizes and prioritizes the personal and professional development of its employees is indicative of its commitment to serving its clients and promoting the well-being of its staff. I'd be very grateful to be apart of this environment."
     }
   ],
   whyMe: [
@@ -26,19 +26,19 @@ export const data = {
       id: 4,
       number: 1,
       title: "Collaboration",
-      description: "By prioritizing collaboration, I am able to produce work that is truly reflective of the vision and values of the team as a whole. I believe it is important to both consistently provide the necessary feedback and recognize each other’s wins. Good vibes keep the wheel rolling!"
+      description: "By prioritizing collaboration, I'm able to produce work that is truly reflective of the vision and values of the team as a whole. I believe it's important to both consistently provide the necessary feedback and recognize each other's wins. Good vibes keep the wheel rolling!"
     },
     {
       id: 5,
       number: 2,
       title: "Simplicity",
-      description: "User experience is the key to retaining existing and welcoming new clients. I believe that if a subpar user experience is unacceptable to me, I must hold my own products to the same high standard."
+      description: "User experience is the key to retaining existing and welcoming new clients. I believe that if a subpar user experience is unacceptable to me, I've got to hold my own products to the same high standard."
     },
     {
       id: 6,
       number: 3,
       title: "Accessibility",
-      description: "The internet is a place that should be accessible to everyone. I continuously strive to expand my knowledge of creating inclusive designs, ensuring that my work can reach the widest possible audience, regardless of any disabilities."
+      description: "The internet is a place that should be accessible to everyone. I continuously strive to expand my knowledge of applying inclusive designs, ensuring that my work can reach the widest possible audience, regardless of disabilities."
     }
   ],
   projects: [
